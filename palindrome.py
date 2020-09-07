76bb1c0b9f5084e413f4ef5f402a7aaed2721bca
Mark Shields <laebshade@gmail.com>
2020-09-06 20:57:58 -0400

    working palindrome kubeless function

--- palindrome.py
+++ palindrome.py
@@ -1,29 +1,5 @@
-from flask import Flask
-from flask_restful import fields, marshal, Resource, Api
-import re
-from typing import Dict
+def palindrome(event, context):
+    import re
 
-
-class Palindrome(Resource):
-    def get(self, word) -> Dict[str, bool]:
-        return palindrome(word)
-
-
-def palindrome(word) -> Dict[str, bool]:
-    normalized = re.sub(r"\W+", "", word).lower()
-    return {
-        "given": word,
-        "normalized": normalized,
-        "palindrome": normalized == normalized[::-1],
-    }
-
-
-def is_palindrome(word) -> bool:
-    return palindrome(word).get("palindrome")
-
-
-if __name__ == "__main__":
-    app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(Palindrome, "/<word>")
-    app.run()
+    normalized = re.sub(r"\W+", "", event["data"].decode("utf-8")).lower()
+    return str(normalized == normalized[::-1])
